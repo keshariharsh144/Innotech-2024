d6689717ffec088080846bbac76d521bf2f5ca95
Harsh Keshari <hkeshari144@gmail.com>
2024-12-10 21:56:51 +0530

    Innotech 2024 project files

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does the 'print()' function do in Python?",
+        answer: "A. Outputs text to the console",
+        options: [
+             "A. Outputs text to the console",
+             "B. Creates a new file",
+             "C. Runs a loop",
+             "D. Closes the program",
+        ]
+    },
+    {
+        numb: 2,
+        question: "How do you insert comments in Python code?",
+        answer: "C. # This is a comment",
+        options: [
+            "A. // This is a comment",
+            "B. /* This is a comment */",
+            "C. # This is a comment",
+            "D. -- This is a comment",
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the correct way to create a function in Python?",
+        answer: "B. def myFunction():",
+        options: [
+            "A. function myFunction()",
+            "B. def myFunction():",
+            "C. create myFunction()",
+            "D. method myFunction()",
+        ]
+    },
+    {
+        numb: 4,
+        question: "How do you create a list in Python?",
+        answer: "C. list = [1, 2, 3]",
+        options: [
+            "A. list = (1, 2, 3)",
+            "B. list = {1, 2, 3}",
+            "C. list = [1, 2, 3]",
+            "D. list = <1, 2, 3>",
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is the output of 'print(3**3)' in Python?",
+        answer: "D. 27",
+        options: [
+            "A. 6",
+            "B. 3",
+            "C. 9",
+            "D. 27",
+        ]
+    },
+];
